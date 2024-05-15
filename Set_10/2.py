23bbded5c7dd39be7222308db08ccf3d3f07dc1e
Bartosz Kasza laptop <b.kasza@student.uw.edu.pl>
2024-05-15 11:20:03 +0200

    done 10

--- Set_10/2.py
+++ Set_10/2.py
@@ -1,15 +1,19 @@
 #%%
-'monte carlo with numba, vanila step'
+'monte carlo with numba, full'
 import numpy as np
 import numba as nb
 import matplotlib.pyplot as plt
 #%%
-N = 8
-be = 4
-de = 1
+N = 40
+be = 10
+de = 0.615
 Dt = be/N
-
+print('de', de)
 # %%
+
+def pi(x, be):
+    sigma2 = 1/(2*np.tanh(be/2))
+    return 1/np.sqrt(2*np.pi*sigma2)*np.exp(-x**2/(2*sigma2))
 
 @nb.jit(nopython = True)
 def rho_free(x, y):
@@ -34,15 +38,20 @@ def monte_carlo_step(x):
 def monte_carlo(x, steps):
     out_avg = []
     out_var = []
+    x0_seq = []
+    xN2_seq = []
     for _ in range(steps):
         newx = monte_carlo_step(x)
         out_avg.append(np.average(newx))
         out_var.append(np.var(newx))
-    return out_avg, out_var
+        x0_seq.append(newx[0])
+        xN2_seq.append(newx[N//2])
+    return out_avg, out_var, x0_seq, xN2_seq
 # %%
 x = np.linspace(-de, de, N)
-nstep = 10000
-out_avg, out_var = monte_carlo(x.copy(), nstep)
+nstep = 1000000
+out_avg, out_var, x0_seq, xN2_seq = monte_carlo(x, nstep)
+# x0_seq, xN2_seq = np.array(x0_seq), np.array(xN2_seq)
 # %%
 plt.plot(range(nstep), out_avg, label = 'avg')
 plt.axhline(y=np.average(out_avg), color='r', linestyle='-', label = 'msc avg avg')
@@ -53,4 +62,9 @@ plt.plot(range(nstep), out_var, label = 'var')
 plt.axhline(y=np.average(out_var), color = 'r',  label = 'mcs avg var')
 plt.legend()
 plt.show()
+# %%
+plt.hist(x0_seq+xN2_seq, density = True, bins = 100, label = 'MC')
+x = np.linspace(-3, 3, N)
+plt.plot(x, pi(x, be), label = 'theory')
+plt.legend()
 # %%
