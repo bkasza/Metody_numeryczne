03066c2f353c01b52e66a2f3a03115970adeecaa
Bartosz Kasza laptop <b.kasza@student.uw.edu.pl>
2025-02-26 15:52:46 +0100

    epsilon maszynowy

--- Seria_1/1.py
+++ Seria_1/1.py
@@ -1,1 +1,12 @@
-"o czym to wszystko XD"
+"""1 dokładność numeryczna"""
+#%%
+'epsilon maszynowy'
+import numpy as np
+
+# %%
+values = [10**(-x) for x in range(30)]
+a = 1
+for x in values:
+    print(a+x, "value:", np.log10(x))
+    
+"epsilon value dla 15"
