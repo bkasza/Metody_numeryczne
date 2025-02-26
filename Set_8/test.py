a93b913aa84e67dfd7231c1ef59bc52c849bfd51
Bartosz Kasza laptop <b.kasza@student.uw.edu.pl>
2025-02-26 15:30:45 +0100

    initial commit

--- Set_8/test.py
+++
@@ -1,9 +0,0 @@
-#%%
-from qiskit import (ClassicalRegister, QuantumRegister, QuantumCircuit, transpile)
-# %%
-q = QuantumRegister(2, name = 'qubits')
-c = ClassicalRegister(2, name = 'bits')
-circuit = QuantumCircuit(q, c)
-# %%
-from qiskit_aer import Aer
-# %%
